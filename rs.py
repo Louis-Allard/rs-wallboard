ed59799db411b5d2ffc76b1f5dab1042a0fe0265
Louis Allard <la.devweb@yahoo.com>
2020-12-02 15:14:48 +0100

    Check twitter api.

--- rs.py
+++ rs.py
@@ -1,12 +1,11 @@
-from bs4 import BeautifulSoup
-from urllib.request import Request, urlopen
+import twitter
+from api import t_key, t_secret, t_bearer, t_token, t_tokensecret
 
+#twitter
 
-url = Request('https://twitter.com/lallard_59', headers={'User-Agent': 'Mozilla/5.0'})
-response = urlopen(url).read()      
-soup = BeautifulSoup(response, 'html.parser')
+api = twitter.Api(consumer_key=t_key, consumer_secret=t_secret, access_token_key=t_token,access_token_secret=t_tokensecret)
 
-title = soup.find('span', class_= 'r-qvutc0')
-    
-print(title)  
+user=api.GetUser(screen_name='lallard_59')
 
+print(user.followers_count)
+print(user.friends_count)
