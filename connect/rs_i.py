c438be5df3fe638f14573035df9ab491c34bb981
Louis Allard <la.devweb@yahoo.com>
2020-12-07 15:28:45 +0100

    Cleaning string.

--- connect/rs_i.py
+++ connect/rs_i.py
@@ -8,7 +8,6 @@ url="https://www.instagram.com/l.allard/"
 response =  requests.get(url)
 soup = BeautifulSoup(response.text, 'lxml')
 raw_script = soup.findAll('script')[3]
-
-script = re.sub("</script>"," ",raw_script.text)
+script = re.sub("<.*?>", "", str(raw_script))
 
 print(script)
