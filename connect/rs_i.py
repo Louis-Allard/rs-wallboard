f2a87cb436ea19f144cff2e82406b599463de4a4
Louis Allard <la.devweb@yahoo.com>
2020-12-07 17:07:56 +0100

    Change image extention.

--- connect/rs_i.py
+++ connect/rs_i.py
@@ -8,14 +8,11 @@ url="https://www.instagram.com/l.allard/"
 
 response =  requests.get(url)
 soup = BeautifulSoup(response.text, 'lxml')
-raw_script = soup.findAll('script')[4]
+raw_script = soup.findAll('script')[3]
 script = re.sub("<.*?>", "", str(raw_script))
 clean = script.replace(";", "").replace("window._sharedData = ", "")
 
 jsondata = json.loads(clean)
 biography = jsondata["entry_data"]["ProfilePage"][0]["graphql"]["user"]
-edge_followed_by = biography["edge_followed_by"]["count"]
-edge_follow =  biography["edge_follow"]["count"]
-
-print(edge_followed_by)
-print(edge_follow)
+followed_by = biography["edge_followed_by"]["count"]
+follow =  biography["edge_follow"]["count"]
