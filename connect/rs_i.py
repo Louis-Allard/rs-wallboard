0b7d486913e124b516b18e35c780c08156b70163
Louis Allard <la.devweb@yahoo.com>
2020-12-07 15:19:49 +0100

    Add re import.

--- connect/rs_i.py
+++ connect/rs_i.py
@@ -1,11 +1,14 @@
 
 import requests
 from bs4 import BeautifulSoup
+import re
 
 url="https://www.instagram.com/l.allard/"
 
 response =  requests.get(url)
-soup = BeautifulSoup(response, 'lxml').text
-script = soup.findAll('script')[3]
+soup = BeautifulSoup(response.text, 'lxml')
+raw_script = soup.findAll('script')[3]
+
+script = re.sub("</script>"," ",raw_script.text)
 
 print(script)
