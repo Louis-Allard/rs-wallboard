178664a0f16d758e6644e1e1fe4e77983d2f8f76
Louis Allard <la.devweb@yahoo.com>
2020-12-07 15:55:49 +0100

    Add json module and found data.

--- connect/rs_i.py
+++ connect/rs_i.py
@@ -2,6 +2,7 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import json
 
 url="https://www.instagram.com/l.allard/"
 
@@ -10,4 +11,11 @@ soup = BeautifulSoup(response.text, 'lxml')
 raw_script = soup.findAll('script')[4]
 script = re.sub("<.*?>", "", str(raw_script))
 clean = script.replace(";", "").replace("window._sharedData = ", "")
-print(clean)
+
+jsondata = json.loads(clean)
+biography = jsondata["entry_data"]["ProfilePage"][0]["graphql"]["user"]
+edge_followed_by = biography["edge_followed_by"]["count"]
+edge_follow =  biography["edge_follow"]["count"]
+
+print(edge_followed_by)
+print(edge_follow)
