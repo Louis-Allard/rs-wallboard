0bbd360c8cc0994ef37d966cc06fef863e312912
Louis Allard <la.devweb@yahoo.com>
2020-12-07 17:09:05 +0100

    First step about window organisation.

--- window.py
+++ window.py
@@ -1,19 +1,33 @@
 from tkinter import *
 from connect.rs_t import fo_user, fr_user
+from connect.rs_i import follow, followed_by
 
 
 win = Tk()
-win.title("rs-wallboard --- alpha 0.0.1")
+win.title("rs-wallboard --- alpha 0.0.2")
 win.geometry("800x600")
 win.configure(bg='white')
 
+
 t_logo = PhotoImage(file="./assets/twitter-logo.png")
-Label(win, image=t_logo, bg='white').pack(anchor="nw", padx=50, pady=5)
+i_logo = PhotoImage(file="./assets/instagram.png")
+
+t_pic = Label(win, image=t_logo, bg='white').pack(anchor="nw", padx=50, pady=5)
 
 t_label1 = Label(win, text="Nombre de followers:", bg='white', fg='black',  font=("arial italic", 18)).pack(anchor='nw', padx=5, pady=5)
 t_data1 = Label(win, text=fo_user, fg='black', bg='white', font=("arial italic", 16)).pack(anchor='nw', padx=80)
 t_label2 = Label(win, text="Nombre de follow:", bg='white', fg='black', font=("arial italic", 18)).pack(anchor='nw', padx=5)
 t_data2 = Label(win, text=fr_user, fg='black', bg='white', font=("arial italic", 16)).pack(anchor='nw', padx=80)
 
-bou1 = Button(win, text='Quitter', command = win.destroy).pack(anchor='s')
+
+i_pic = Label(win, image=i_logo, bg='white').pack(anchor="ne", padx=100,pady=5)
+
+i_label1 = Label(win, text="Nombre de followers:", bg='white', fg='black',  font=("arial italic", 18)).pack(anchor='ne', padx=5, pady=5)
+i_data1 = Label(win, text=followed_by, fg='black', bg='white', font=("arial italic", 16)).pack(anchor='ne')
+i_label2 = Label(win, text="Nombre de follow:", bg='white', fg='black', font=("arial italic", 18)).pack(anchor='ne', padx=5)
+i_data2 = Label(win, text=follow, fg='black', bg='white', font=("arial italic", 16)).pack(anchor='ne')
+
+
+btn = Button(win, text='Quitter', command = win.destroy).pack(anchor='s')
+
 win.mainloop()
