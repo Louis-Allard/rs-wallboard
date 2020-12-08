5fc189c46e46942b6ebd55c96976efa21be0a41a
Louis Allard <la.devweb@yahoo.com>
2020-12-08 17:24:44 +0100

    Add frames.

--- window.py
+++ window.py
@@ -4,30 +4,36 @@ from connect.rs_i import follow, followed_by
 
 
 win = Tk()
-win.title("rs-wallboard --- alpha 0.0.2")
-win.geometry("800x600")
+win.title("rs-wallboard --- alpha 0.0.3")
+win.geometry("1080x720")
+win.minsize(800,600)
 win.configure(bg='white')
 
+frame    = Frame(win, bg='grey', bd=1, relief=SUNKEN, width='300')
+frame2   = Frame(win, bg='yellow', bd=1, relief=SUNKEN, width='300')
 
-t_logo = PhotoImage(file="./assets/twitter-logo.png")
-i_logo = PhotoImage(file="./assets/instagram.png")
+frame5   = Frame(win, bg='blue', bd=1, relief=SUNKEN)
 
-t_pic = Label(win, image=t_logo, bg='white').pack(anchor="nw", padx=50, pady=10)
+t_logo   = PhotoImage(file="./assets/twitter-logo.png")
+i_logo   = PhotoImage(file="./assets/instagram.png")
 
-t_label1 = Label(win, text="Nombre de followers:", bg='white', fg='black',  font=("arial italic", 18)).pack(anchor='nw', padx=5, pady=5)
-t_data1 = Label(win, text=fo_user, fg='black', bg='white', font=("arial italic", 16)).pack(anchor='nw', padx=80)
-t_label2 = Label(win, text="Nombre de follow:", bg='white', fg='black', font=("arial italic", 18)).pack(anchor='nw', padx=5)
-t_data2 = Label(win, text=fr_user, fg='black', bg='white', font=("arial italic", 16)).pack(anchor='nw', padx=80)
-
-
-i_pic = Label(win, image=i_logo, bg='white').pack(anchor="nw", padx=50,pady=20)
-
-i_label1 = Label(win, text="Nombre de followers:", bg='white', fg='black',  font=("arial italic", 18)).pack(anchor='w', padx=5, pady=5)
-i_data1 = Label(win, text=followed_by, fg='black', bg='white', font=("arial italic", 16)).pack(anchor='w',padx=80)
-i_label2 = Label(win, text="Nombre de follow:", bg='white', fg='black', font=("arial italic", 18)).pack(anchor='w', padx=5)
-i_data2 = Label(win, text=follow, fg='black', bg='white', font=("arial italic", 16)).pack(anchor='w', padx=80)
+t_pic    = Label(frame, image=t_logo, bg='white').pack()
+t_label1 = Label(frame, text="Nombre de followers:", bg='white', fg='black',  font=("arial italic", 18)).pack()
+t_data1  = Label(frame, text=fo_user, fg='black', bg='white', font=("arial italic", 16)).pack()
+t_label2 = Label(frame, text="Nombre de follow:", bg='white', fg='black', font=("arial italic", 18)).pack()
+t_data2  = Label(frame, text=fr_user, fg='black', bg='white', font=("arial italic", 16)).pack()
 
 
-btn = Button(win, text='Quitter', command = win.destroy).pack(anchor='s')
+i_pic    = Label(frame2, image=i_logo, bg='white').pack()
+i_label1 = Label(frame2, text="Nombre de followers:", bg='white', fg='black',  font=("arial italic", 18)).pack()
+i_data1  = Label(frame2, text=followed_by, fg='black', bg='white', font=("arial italic", 16)).pack()
+i_label2 = Label(frame2, text="Nombre de follow:", bg='white', fg='black', font=("arial italic", 18)).pack()
+i_data2  = Label(frame2, text=follow, fg='black', bg='white', font=("arial italic", 16)).pack()
 
+
+btn      = Button(frame5, text='Quitter', command=win.destroy).pack()
+
+frame.pack(expand='YES')
+frame2.pack(expand='YES')
+frame5.pack(expand='YEs')
 win.mainloop()
