a3edeb4500c4d8e6cdf6034d6a0de47fc5c3ae8b
Louis Allard <la.devweb@yahoo.com>
2020-12-08 17:41:05 +0100

    Fix frames.

--- window.py
+++ window.py
@@ -5,14 +5,14 @@ from connect.rs_i import follow, followed_by
 
 win = Tk()
 win.title("rs-wallboard --- alpha 0.0.3")
-win.geometry("1080x720")
-win.minsize(800,600)
+win.geometry("1280x400")
+win.minsize(1080,300)
 win.configure(bg='white')
 
-frame    = Frame(win, bg='grey', bd=1, relief=SUNKEN, width='300')
-frame2   = Frame(win, bg='yellow', bd=1, relief=SUNKEN, width='300')
-
-frame5   = Frame(win, bg='blue', bd=1, relief=SUNKEN)
+frame    = Frame(win, bg='white')
+frame2   = Frame(win, bg='white')
+frame3   = Frame(win, bg='white')
+frame4   = Frame(win, bg='white')
 
 t_logo   = PhotoImage(file="./assets/twitter-logo.png")
 i_logo   = PhotoImage(file="./assets/instagram.png")
@@ -30,10 +30,28 @@ i_data1  = Label(frame2, text=followed_by, fg='black', bg='white', font=("arial 
 i_label2 = Label(frame2, text="Nombre de follow:", bg='white', fg='black', font=("arial italic", 18)).pack()
 i_data2  = Label(frame2, text=follow, fg='black', bg='white', font=("arial italic", 16)).pack()
 
+'''
+TESTS
+'''
+t_pic    = Label(frame3, image=t_logo, bg='white').pack()
+t_label1 = Label(frame3, text="Nombre de followers:", bg='white', fg='black',  font=("arial italic", 18)).pack()
+t_data1  = Label(frame3, text=fo_user, fg='black', bg='white', font=("arial italic", 16)).pack()
+t_label2 = Label(frame3, text="Nombre de follow:", bg='white', fg='black', font=("arial italic", 18)).pack()
+t_data2  = Label(frame3, text=fr_user, fg='black', bg='white', font=("arial italic", 16)).pack()
 
-btn      = Button(frame5, text='Quitter', command=win.destroy).pack()
+i_pic    = Label(frame4, image=i_logo, bg='white').pack()
+i_label1 = Label(frame4, text="Nombre de followers:", bg='white', fg='black',  font=("arial italic", 18)).pack()
+i_data1  = Label(frame4, text=followed_by, fg='black', bg='white', font=("arial italic", 16)).pack()
+i_label2 = Label(frame4, text="Nombre de follow:", bg='white', fg='black', font=("arial italic", 18)).pack()
+i_data2  = Label(frame4, text=follow, fg='black', bg='white', font=("arial italic", 16)).pack()
+
+'''
+FIN TESTS
+'''
 
 frame.pack(expand='YES', side=LEFT)
 frame2.pack(expand='YES', side=LEFT)
-frame5.pack(expand='YEs')
+frame3.pack(expand='YES', side=RIGHT)
+frame4.pack(expand='YES', side=RIGHT)
+
 win.mainloop()
