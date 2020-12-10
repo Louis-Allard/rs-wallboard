e91bfa00cfcb6f48fb1030170ed0eda6c8f7f985
Louis Allard <la.devweb@yahoo.com>
2020-12-10 17:43:15 +0100

    Add variable.

--- __main__.py
+++ __main__.py
@@ -9,10 +9,12 @@ win.geometry("1280x400")
 win.minsize(1080,300)
 win.configure(bg='white')
 
-frame    = Frame(win, bg='white')
-frame2   = Frame(win, bg='white')
-frame3   = Frame(win, bg='white')
-frame4   = Frame(win, bg='white')
+bgcolor  = 'white'
+
+frame    = Frame(win, bg=bgcolor)
+frame2   = Frame(win, bg=bgcolor)
+frame3   = Frame(win, bg=bgcolor)
+frame4   = Frame(win, bg=bgcolor)
 
 #logos
 t_logo   = PhotoImage(file="rs-wallboard/assets/twitter.png")
@@ -20,25 +22,25 @@ i_logo   = PhotoImage(file="rs-wallboard/assets/instagram.png")
 l_logo   = PhotoImage(file="rs-wallboard/assets/linkedin.png")
 
 #Twitter
-t_pic    = Label(frame, image=t_logo, bg='white').pack()
-t_label1 = Label(frame, text="Nombre de followers:", bg='white', fg='black',  font=("arial italic", 18)).pack()
-t_data1  = Label(frame, text=fo_user, fg='black', bg='white', font=("arial italic", 16)).pack()
-t_label2 = Label(frame, text="Nombre de follow:", bg='white', fg='black', font=("arial italic", 18)).pack()
-t_data2  = Label(frame, text=fr_user, fg='black', bg='white', font=("arial italic", 16)).pack()
+t_pic    = Label(frame, image=t_logo, bg=bgcolor).pack()
+t_label1 = Label(frame, text="Nombre de followers:", bg=bgcolor, fg='black',  font=("arial italic", 18)).pack()
+t_data1  = Label(frame, text=fo_user, fg='black', bg=bgcolor, font=("arial italic", 16)).pack()
+t_label2 = Label(frame, text="Nombre de follow:", bg=bgcolor, fg='black', font=("arial italic", 18)).pack()
+t_data2  = Label(frame, text=fr_user, fg='black', bg=bgcolor, font=("arial italic", 16)).pack()
 
 #Instagram
-i_pic    = Label(frame2, image=i_logo, bg='white').pack()
-i_label1 = Label(frame2, text="Nombre de followers:", bg='white', fg='black',  font=("arial italic", 18)).pack()
-i_data1  = Label(frame2, text=followed_by, fg='black', bg='white', font=("arial italic", 16)).pack()
-i_label2 = Label(frame2, text="Nombre de follow:", bg='white', fg='black', font=("arial italic", 18)).pack()
-i_data2  = Label(frame2, text=follow, fg='black', bg='white', font=("arial italic", 16)).pack()
+i_pic    = Label(frame2, image=i_logo, bg=bgcolor).pack()
+i_label1 = Label(frame2, text="Nombre de followers:", bg=bgcolor, fg='black',  font=("arial italic", 18)).pack()
+i_data1  = Label(frame2, text=followed_by, fg='black', bg=bgcolor, font=("arial italic", 16)).pack()
+i_label2 = Label(frame2, text="Nombre de follow:", bg=bgcolor, fg='black', font=("arial italic", 18)).pack()
+i_data2  = Label(frame2, text=follow, fg='black', bg=bgcolor, font=("arial italic", 16)).pack()
 
 #Linkedin
-l_pic    = Label(frame2, image=i_logo, bg='white').pack()
-l_label1 = Label(frame2, text="Nombre de followers:", bg='white', fg='black',  font=("arial italic", 18)).pack()
-l_data1  = Label(frame2, text=followed_by, fg='black', bg='white', font=("arial italic", 16)).pack()
-l_label2 = Label(frame2, text="Nombre de follow:", bg='white', fg='black', font=("arial italic", 18)).pack()
-l_data2  = Label(frame2, text=follow, fg='black', bg='white', font=("arial italic", 16)).pack()
+l_pic    = Label(frame2, image=l_logo, bg=bgcolor).pack()
+l_label1 = Label(frame2, text="Nombre de followers:", bg=bgcolor, fg='black',  font=("arial italic", 18)).pack()
+l_data1  = Label(frame2, text=followed_by, fg='black', bg=bgcolor, font=("arial italic", 16)).pack()
+l_label2 = Label(frame2, text="Nombre de follow:", bg=bgcolor, fg='black', font=("arial italic", 18)).pack()
+l_data2  = Label(frame2, text=follow, fg='black', bg=bgcolor, font=("arial italic", 16)).pack()
 
 #Facebook
 
