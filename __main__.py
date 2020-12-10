9bdb9252cf912f9353e2825289c1f48e5ebe3786
Louis Allard <la.devweb@yahoo.com>
2020-12-10 17:26:35 +0100

    Add comments.

--- __main__.py
+++ __main__.py
@@ -4,7 +4,7 @@ from .connect.rs_i import follow, followed_by
 
 
 win = Tk()
-win.title("rs-wallboard --- alpha 0.0.3")
+win.title("rs-wallboard --- alpha 0.0.5")
 win.geometry("1280x400")
 win.minsize(1080,300)
 win.configure(bg='white')
@@ -14,21 +14,28 @@ frame2   = Frame(win, bg='white')
 frame3   = Frame(win, bg='white')
 frame4   = Frame(win, bg='white')
 
-t_logo   = PhotoImage(file="rs-wallboard/assets/twitter-logo.png")
+#logos
+t_logo   = PhotoImage(file="rs-wallboard/assets/twitter.png")
 i_logo   = PhotoImage(file="rs-wallboard/assets/instagram.png")
+l_logo   = PhotoImage(file="rs-wallboard/assets/linkedin.png")
 
+#Twitter
 t_pic    = Label(frame, image=t_logo, bg='white').pack()
 t_label1 = Label(frame, text="Nombre de followers:", bg='white', fg='black',  font=("arial italic", 18)).pack()
 t_data1  = Label(frame, text=fo_user, fg='black', bg='white', font=("arial italic", 16)).pack()
 t_label2 = Label(frame, text="Nombre de follow:", bg='white', fg='black', font=("arial italic", 18)).pack()
 t_data2  = Label(frame, text=fr_user, fg='black', bg='white', font=("arial italic", 16)).pack()
 
-
+#Instagram
 i_pic    = Label(frame2, image=i_logo, bg='white').pack()
 i_label1 = Label(frame2, text="Nombre de followers:", bg='white', fg='black',  font=("arial italic", 18)).pack()
 i_data1  = Label(frame2, text=followed_by, fg='black', bg='white', font=("arial italic", 16)).pack()
 i_label2 = Label(frame2, text="Nombre de follow:", bg='white', fg='black', font=("arial italic", 18)).pack()
 i_data2  = Label(frame2, text=follow, fg='black', bg='white', font=("arial italic", 16)).pack()
+
+#Linkedin
+
+#Facebook
 
 '''
 TESTS
