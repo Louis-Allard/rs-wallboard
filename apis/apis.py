7c17b56bfd28705c07f05e8897445d290d05d2d5
Louis Allard <la.devweb@yahoo.com>
2020-12-10 17:33:22 +0100

    Add linkedin api elements.

--- apis/apis.py
+++ apis/apis.py
@@ -3,3 +3,9 @@ t_key="azertyuiop"
 t_secret="azertyuiop"
 t_token="azertyuiop"
 t_tokensecret="azertyuiop"
+
+l_custumerkey = 'azertyuiop'     # This is api_key
+l_custumersecret = 'azertyuiop'   # This is secret_key
+l_usertoken = 'azertyuiop   # This is oauth_token
+l_usersecret = 'azertyuiop'   # This is oauth_secret
+l_returnurl = 'azertyuiop'
