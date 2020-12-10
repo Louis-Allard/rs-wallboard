0e002b3bfd83ec1be15d54bb7f5381e7587f4509
Louis Allard <la.devweb@yahoo.com>
2020-12-10 17:50:54 +0100

    Add Facebook into main.

--- __main__.py
+++ __main__.py
@@ -20,6 +20,7 @@ frame4   = Frame(win, bg=bgcolor)
 t_logo   = PhotoImage(file="rs-wallboard/assets/twitter.png")
 i_logo   = PhotoImage(file="rs-wallboard/assets/instagram.png")
 l_logo   = PhotoImage(file="rs-wallboard/assets/linkedin.png")
+f_logo   = PhotoImage(file="rs-wallboard/assets/facebook.png")
 
 #Twitter
 t_pic    = Label(frame, image=t_logo, bg=bgcolor).pack()
@@ -36,32 +37,19 @@ i_label2 = Label(frame2, text="Nombre de follow:", bg=bgcolor, fg='black', font=
 i_data2  = Label(frame2, text=follow, fg='black', bg=bgcolor, font=("arial italic", 16)).pack()
 
 #Linkedin
-l_pic    = Label(frame2, image=l_logo, bg=bgcolor).pack()
-l_label1 = Label(frame2, text="Nombre de followers:", bg=bgcolor, fg='black',  font=("arial italic", 18)).pack()
-l_data1  = Label(frame2, text=followed_by, fg='black', bg=bgcolor, font=("arial italic", 16)).pack()
-l_label2 = Label(frame2, text="Nombre de follow:", bg=bgcolor, fg='black', font=("arial italic", 18)).pack()
-l_data2  = Label(frame2, text=follow, fg='black', bg=bgcolor, font=("arial italic", 16)).pack()
+l_pic    = Label(frame3, image=l_logo, bg=bgcolor).pack()
+l_label1 = Label(frame3, text="Nombre de relations:", bg=bgcolor, fg='black',  font=("arial italic", 18)).pack()
+l_data1  = Label(frame3, text=followed_by, fg='black', bg=bgcolor, font=("arial italic", 16)).pack()
+l_label2 = Label(frame3, text="Nombre de vues:", bg=bgcolor, fg='black', font=("arial italic", 18)).pack()
+l_data2  = Label(frame3, text=follow, fg='black', bg=bgcolor, font=("arial italic", 16)).pack()
 
 #Facebook
+f_pic    = Label(frame4, image=t_logo, bg='white').pack()
+f_label1 = Label(frame4, text="Nombre de followers:", bg='white', fg='black',  font=("arial italic", 18)).pack()
+f_data1  = Label(frame4, text=fo_user, fg='black', bg='white', font=("arial italic", 16)).pack()
+f_label2 = Label(frame4, text="Nombre de follow:", bg='white', fg='black', font=("arial italic", 18)).pack()
+f_data2  = Label(frame4, text=fr_user, fg='black', bg='white', font=("arial italic", 16)).pack()
 
-'''
-TESTS
-'''
-t_pic    = Label(frame3, image=t_logo, bg='white').pack()
-t_label1 = Label(frame3, text="Nombre de followers:", bg='white', fg='black',  font=("arial italic", 18)).pack()
-t_data1  = Label(frame3, text=fo_user, fg='black', bg='white', font=("arial italic", 16)).pack()
-t_label2 = Label(frame3, text="Nombre de follow:", bg='white', fg='black', font=("arial italic", 18)).pack()
-t_data2  = Label(frame3, text=fr_user, fg='black', bg='white', font=("arial italic", 16)).pack()
-
-i_pic    = Label(frame4, image=i_logo, bg='white').pack()
-i_label1 = Label(frame4, text="Nombre de followers:", bg='white', fg='black',  font=("arial italic", 18)).pack()
-i_data1  = Label(frame4, text=followed_by, fg='black', bg='white', font=("arial italic", 16)).pack()
-i_label2 = Label(frame4, text="Nombre de follow:", bg='white', fg='black', font=("arial italic", 18)).pack()
-i_data2  = Label(frame4, text=follow, fg='black', bg='white', font=("arial italic", 16)).pack()
-
-'''
-FIN TESTS
-'''
 
 frame.pack(expand='YES', side=LEFT)
 frame2.pack(expand='YES', side=LEFT)
